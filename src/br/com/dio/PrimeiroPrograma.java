67235b5a8ea6fc92bafe26c6c1aa7fb28c06d929
stemorizinho <estevaoroyal14@gmail.com>
2022-10-09 18:29:33 -0300

    mostrar

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+        /*int a = 3;
+        int b = 5;
+        System.out.println("hello world"    +   (a+b));*/
+    }
+}
+
+
+
